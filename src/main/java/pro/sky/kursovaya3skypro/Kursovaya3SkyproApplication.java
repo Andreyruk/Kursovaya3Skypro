114d9a9f371901d46de221bc068b3003f75de618
MakTub <7andreyru@mail.ru>
2023-03-11 15:42:27 +0500

    Курсовая работа 3-его курса

---
+++ src/main/java/pro/sky/kursovaya3skypro/Kursovaya3SkyproApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.kursovaya3skypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kursovaya3SkyproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kursovaya3SkyproApplication.class, args);
+	}
+
+}
